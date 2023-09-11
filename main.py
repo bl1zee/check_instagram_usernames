fffa2c3e17bf77d26a04484fdfc350b28cfe068e
bl1zee <ciber.seguridad.blize@gmail.com>
2023-09-11 07:25:06 +0200

    probando otro metodo, desde el login

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from bs4 import BeautifulSoup  
 import requests
 
-website = 'https://www.vulnhub.com/'
-result = requests.get(website)
-content = result.text
+website = 'https://www.instagram.com/accounts/emailsignup/'
+params = {'emailOrPhone':'test.sadfadsfsdafgsdfgsdfg@gmail.com', 'fullName':'test', 'username':'paabloo_mtn', 'password':'Adfer:1@ddfD'}
+response = requests.post(website, data = params)
+content = response.text
 
 soup = BeautifulSoup(content, 'lxml')
 
-print(soup.prettify())
+box = soup.find_all('div', class_="_aaht")
+filter = box.find('p').get_text()
+
+print(filter)
